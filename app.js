1950e1fb361cef3fe63b214e605c6494a32280e6
ColouMods <32771001+ColouMods@users.noreply.github.com>
2017-10-13 14:34:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,4 +11,40 @@ client.on('message', message => {
   }
 });
 
+client.on('message', message => {
+  if (message.content === '@PetBot How are you today?') {
+    message.reply('Fine, a bit bored. I wish something exciting would happen...');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === '@PetBot Wake up!') {
+    message.reply('I *am* awake');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === '@PetBot What is the current HA release') {
+    message.reply('The current release is Beta 4.7');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === '@PetBot SHUT UP!') {
+    message.reply('\*sniff* Will do...master');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === '@PetBot Play my mod') {
+    message.reply('I already did.');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === '@PetBot You talk to much') {
+    message.reply('Correct');
+  }
+});
+
 client.login(process.env.BOT_TOKEN);

f756c977487e6ef297d0db20d9c654dd78a6f482
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-22 13:35:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,111 +6,84 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content === '@PetBot') {
-    message.reply('You called?');
-  }
-});
+  if (message.content.match(/@PetBot/i))
 
-client.on('message', message => {
-  if (message.content === '@PetBot How are you today?') {
-    message.reply('Fine, a bit bored. I wish something exciting would happen...');
+  if (message.content.match(/How are you today?/i)
+  {
+  message.reply('Fine, a bit bored. I wish something exciting would happen...');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot Wake up!') {
+  else if (message.content.match(/Wake up!/i)
+  {
     message.reply('I *am* awake');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot What is the current HA release?') {
+  else if (message.content.match(/What is the current HA release?/i)
+  {
     message.reply('The current release is Beta 4.8');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot SHUT UP!') {
-    message.reply('*sniff* Will do...master');
+  else if (message.content.match(/SHUT UP!/i)
+  {
+    message.reply('*snelse iff* Will do...master');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot Play my mod') {
+  else if (message.content.match(/Play my mod/i)
+  {
     message.reply('I already did.');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot You talk too much') {
+  else if (message.content.match(/You talk too much/i)
+  {
     message.reply('I think *you* talk too much!');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot Do an impression of a robot') {
+  else if (message.content.match(/Do an impression of a robot/i)
+  {
     message.reply('*Beep Boop* I am a robot *Boop Beep*');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot, I am your father') {
+  else if (message.content.match(/I am your father/i) {
     message.reply('NOOOOOOOOOOOOOOOO!');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot Hi') {
+  else if (message.content.match(/Hi/i) {
     message.reply('Hello');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot You suck') {
+  else if (message.content.match(/You suck/i) {
     message.reply('*Sniff*');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot Can I see your source code?') {
+  else if (message.content.match(/Can I see your source code?/i) {
     message.reply('No way! You want to *copy* my source code?');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot You are a great bot!') {
+  else if (message.content.match(/You are a great bot!/i) {
     message.reply('Thanks!');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot Who is the best member of Buzz Team?') {
+  else if (message.content.match(/Who is the best member of Buzz Team?/i) {
     message.reply('Colou');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot Who is the best member of Donut Team?') {
+  else if (message.content.match(/Who is the best member of Donut Team?/i) {
     message.reply('Kenny Giles');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot Who is the best member of Bagel Team?') {
+  else if (message.content.match(/Who is the best member of Bagel Team?/i) {
     message.reply('Genny Kiles');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot What is your favourite mod?') {
+  else if (message.content.match(/What is your favourite mod?/i) {
     message.reply('Sunday Drive, made by **@Colou**');
   }
-});
 
-client.on('message', message => {
-  if (message.content === '@PetBot Command List') {
+  else if (message.content.match(/Command List/i) {
     message.reply('Here is the list of commands I can respond to: ```@PetBot``` ```@PetBot How are you today?``` ```@PetBot Wake up!``` ```@PetBot What is the current HA release?``` ```@PetBot SHUT UP!``` ```@PetBot Play my mod``` ```@PetBot You talk too much``` ```@PetBot Command List``` ```@PetBot Do an impression of a robot``` ```@PetBot, I am your father``` ```@PetBot Hi``` ```@PetBot You suck``` ```@PetBot Can I see your source code?``` ```@PetBot You are a great bot!``` ```@PetBot Who is the best member of Buzz Team?``` ```@PetBot Who is the best member of Donut Team?``` ```@PetBot Who is the best member of Bagel Team?``` ```@PetBot What is your favorite mod?```');
   }
+  
+  else {
+	message.reply('You called?');
+  }
 });
-
-client.login(process.env.BOT_TOKEN);
